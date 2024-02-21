3d8605ed79ff3d514ee86642f4ca8471ec0d4373
Ajax2151 <ajackson2511@gmail.com>
2024-02-21 13:51:10 -0600

    Definition of playRound() started Defined initial playRound(), need to debug

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
-const ROCK = 0;
-const PAPER = 1;
-const SCISSORS = 2;
-
-// Get player choice - rock, paper, or scissors
 // Get computer choice - rock, paper, or scissors
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3)
+    let choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        return "Rock"
+    } else if (choice == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
 }
+
+// Play a round
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+
+    // switch (player, computer) {
+    //     case player == computer:
+    //         return "It's a tie!";
+    //     break;
+
+    //     case player == "rock" && computer == "paper":
+    //         return "You Lose! Paper beats Rock.";
+    //     break;
+
+    //     case player == "paper" && computer == "scissors":
+    //         return "You Lose! Scissors beat Paper.";
+    //     break;
+
+    //     case player == "scissors" && computer == "rock":
+    //         return "You Lose! Rock beats Scissors.";
+    //     break;
+
+    //     case player == "rock" && computer == "scissors":
+    //         return "You Win! Rock beats Scissors.";
+    //     break;
+
+    //     case player == "paper" && computer == "rock":
+    //         return "You Win! Paper beats Rock.";
+    //     break;
+
+    //     case player == "scissors" && computer == "paper":
+    //         return "You Win! Scissors beat Paper."
+    //     break;
+    // }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
 // Evaluate winner based on hierarchy - paper beats rock, rock beats scissors, scissors beat paper
 // Notify player of result
