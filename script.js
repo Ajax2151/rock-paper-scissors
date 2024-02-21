2942505f457670e00b2decabc37f597bd2404159
Ajax2151 <ajackson2511@gmail.com>
2024-02-21 13:18:20 -0600

    Defined Computer Choice Defined function to get computer choice by random selection

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const ROCK = 0;
+const PAPER = 1;
+const SCISSORS = 2;
+
 // Get player choice - rock, paper, or scissors
 // Get computer choice - rock, paper, or scissors
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3)
+}
 // Evaluate winner based on hierarchy - paper beats rock, rock beats scissors, scissors beat paper
 // Notify player of result
