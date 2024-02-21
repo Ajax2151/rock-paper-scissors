3bdebae5544e3d2061033e4a978874f921db5254
Ajax2151 <ajackson2511@gmail.com>
2024-02-21 13:08:53 -0600

    Outlined desired functionality Broke down desired functionality and steps to pseudocode in script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Get player choice - rock, paper, or scissors
+// Get computer choice - rock, paper, or scissors
+// Evaluate winner based on hierarchy - paper beats rock, rock beats scissors, scissors beat paper
+// Notify player of result
